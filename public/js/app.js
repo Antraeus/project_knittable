3bd3acfe2ec74188029b8a0a8103b4a963a00536
Molly Kent <kent.molly@gmail.com>
2015-04-21 09:16:40 -0700

    initial commit. scaffolded Sweater object constructor

---
+++ public/js/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+$(function() {
+  var Sweater = function(sweaterName, options) {
+    this.sweaterName = ;
+    this.directionUp = ;
+    this.shoulderStyle = ;
+    this.neckStyle = ;
+    this.shaping = ;
+    this.length = ;
+    this.knitFlat = ;
+    this.ease = ;
+    this.hem = ;
+    this.stitchGauge = ;
+    this.rowGauge = ;
+  }
+});
