7f21923a64ee129cb3f071415208b88d62acab11
Molly Kent <kent.molly@gmail.com>
2015-04-21 12:23:13 -0700

    Close #11 build sweater constructor

---
+++ public/js/Sweater.js
@@ -0,0 +1,82 @@
+$(function() {
+  var stGauge;
+  var rGauge;
+  var sweaterTitle;
+  var direction;
+  var shoulders;
+  var crew;
+  var shaped;
+  var hemLong;
+  var ease;
+  var counter = 0;
+  $("#save-new-design").submit(function () { // listens for the form to submit
+    event.preventDefault(); //prevents page refresh
+    stGauge = $('#stitch-gauge').val();
+    rGauge = $('#row-gauge').val();
+    sweaterTitle = $('#project-name').val();
+    if ($('#up').prop('checked')) {
+      direction = true;
+    } else {
+      direction = false;
+    }
+    if ($('#yoke').prop('checked')) {
+      shoulders = 'yoke';
+    } 
+    if ($('#set-in').prop('checked')) {
+      shoulders = 'set-in';
+    }
+    if ($('#raglan').prop('checked')) {
+      shoulders = 'raglan';
+    }
+    if ($('#crew').prop('checked')) {
+      crew = true;
+    } 
+    if ($('#vee').prop('checked')) {
+      crew = false;
+    }
+    if ($('#yes-shape').prop('checked')) {
+      shaped = true;
+    } else {
+      shaped = false;
+    }
+    if ($('#yes-hips').prop('checked')) {
+      hemLong = true;
+    } else {
+      hemLong = false;
+    }
+    if ($('#close').prop('checked')) {
+      ease = 2;
+    } else {
+      ease = 4;
+    }
+    counter++
+    console.log('St Gauge: ' + stGauge);
+    console.log('Row Gauge: ' + rGauge);
+    console.log('Sweater Title: ' + sweaterTitle);
+    console.log('Direction Up: ' + direction);
+    console.log('Shoulders: ' + shoulders);
+    console.log('Crew neck: ' + crew);
+    console.log('Shaped: ' + shaped);
+    console.log('Long: ' + hemLong);
+    console.log('Fit: ' + ease);
+    console.log('Counter: ' + counter);
+
+    var sweater = new Sweater(sweaterTitle);
+    console.log(sweater);
+     // sets entered name to $userName
+    // localStorage.setItem('sweater', $userName); // puts $userName in localStorage with a key of 'knitter'
+    // $('#knitter-name').val(''); // removed entered text from form input
+  })
+
+  var Sweater = function(sweaterName, options) {
+    this.sweaterName = sweaterTitle;
+    this.directionUp = direction;
+    this.shoulderStyle = shoulders;
+    this.neckStyle = crew;
+    this.shaping = shaped;
+    this.length = hemLong;
+    this.fit = ease;
+    this.stitchGauge = stGauge;
+    this.rowGauge = rGauge;
+  };
+});
