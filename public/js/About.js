1069d5d6745c6c5c1cbed10710c24339b09c245b
Philip A. Hirt <phirt13@gmail.com>
2015-04-24 02:27:37 -0700

    Close #160 final_styles

---
+++ public/js/About.js
@@ -0,0 +1,37 @@
+$(function() {
+  $('#molly-picture').click(function() {
+    $('#molly-bio').slideDown();
+  });
+});
+
+$(function() {
+ $('#hide-molly').click(function() {
+    $('#molly-bio').slideUp();
+ });
+});
+
+$(function() {
+  $('#phil-bio').hide();
+  $('#phil-picture').click(function() {
+    $('#phil-bio').slideDown();
+  });
+});
+
+$(function() {
+  $('#hide-phil').click(function() {
+    $('#phil-bio').slideUp();
+  });
+});
+
+$(function() {
+  $('#aristide-bio').hide();
+  $('#aristide-picture').click(function() {
+    $('#aristide-bio').slideDown();
+  });
+});
+
+$(function() {
+  $('#hide-aristide').click(function() {
+    $('#aristide-bio').slideUp();
+  });
+});
