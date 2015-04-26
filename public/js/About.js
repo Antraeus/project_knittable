3f6d567a7dbe461345944d1ae63c1a49684e9a54
Molly Kent <kent.molly@gmail.com>
2015-04-26 16:52:20 -0700

    Close #1 remove stylesheets

--- public/js/About.js
+++ public/js/About.js
@@ -9,29 +9,3 @@ $(function() {
     $('#molly-bio').slideUp();
  });
 });
-
-$(function() {
-  $('#phil-bio').hide();
-  $('#phil-picture').click(function() {
-    $('#phil-bio').slideDown();
-  });
-});
-
-$(function() {
-  $('#hide-phil').click(function() {
-    $('#phil-bio').slideUp();
-  });
-});
-
-$(function() {
-  $('#aristide-bio').hide();
-  $('#aristide-picture').click(function() {
-    $('#aristide-bio').slideDown();
-  });
-});
-
-$(function() {
-  $('#hide-aristide').click(function() {
-    $('#aristide-bio').slideUp();
-  });
-});
